d53b4be058d6fab69a6101446c34f64688228780
jluongg <jimmy.luong.pro@gmail.com>
2024-04-27 23:42:13 +0200

    first commit

---
+++ backend/main.py
@@ -0,0 +1,20 @@
+from typing import Union
+
+from fastapi import FastAPI
+from pydantic import BaseModel
+import uvicorn
+
+app = FastAPI()
+
+class Item(BaseModel):
+    name: str
+
+@app.post("/ping")
+async def create_item(item: Item):
+    if item.name.lower() == "ping":
+        return "pong"
+    return item
+
+    
+if __name__ == '__main__':
+    uvicorn.run(app, port=8080, host='localhost')
